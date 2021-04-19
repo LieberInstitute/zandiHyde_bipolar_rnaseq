c0de26e4754b7940d02959c384430d52de3fd8f0
lahuuki <lahuuki@gmail.com>
2021-04-19 12:36:32 -0400

    Add spearman cor annotation

--- case_control/de_results_compare_regions.R
+++ case_control/de_results_compare_regions.R
@@ -21,17 +21,25 @@ table(statOut$`FDR < 0.05`)
 # Both Amygdala     sACC     None 
 # 67      253     1768   759277 
 
+## spearman cor
+region_cor <- statOut %>% 
+  group_by(Type) %>%
+  summarize(cor = cor(t_Amyg, t_sACC, method = "spearman")) %>%
+  mutate(cor_anno = paste0("rho==", format(round(cor, 2), nsmall = 2)))
+
 fdr_colors <- c(None = "gray", sACC = "skyblue3", Amygdala = "orange", Both = "purple")
 
-t_stat_scatter <- ggplot(statOut, aes(x = t_Amyg, y = t_sACC, color = `FDR < 0.05`)) +
-  geom_point(size = 0.5, alpha = 0.5) +
+t_stat_scatter <- ggplot(statOut, aes(x = t_Amyg, y = t_sACC)) +
+  geom_point(aes(color = `FDR < 0.05`), size = 0.5, alpha = 0.5) +
   facet_wrap(~Type) +
   labs(x = "t-stat Amygdala", y = "t-stat sACC") +
   scale_color_manual(values = fdr_colors) +
-  theme_bw(base_size = 15)
+  geom_text(data = region_cor, aes(x = -4.25, y = 6, label = cor_anno), parse = TRUE) +
+  theme_bw(base_size = 15) +
+  NULL
 
-ggsave(t_stat_scatter, filename = here("case_control","deStats_region_t_plots.pdf"))
 ggsave(t_stat_scatter, filename = here("case_control","deStats_region_t_plots.png"))
+ggsave(t_stat_scatter, filename = here("case_control","deStats_region_t_plots.pdf"))
 
 # sgejobs::job_single('de_results_compare_regions', create_shell = TRUE, queue= 'bluejay', memory = '10G', command = "Rscript de_results_compare_regions.R")
 ## Reproducibility information
