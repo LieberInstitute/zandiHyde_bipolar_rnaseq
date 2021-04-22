4e47b145f1052248fc5196eb2dffe0b9f437af2f
lahuuki <lahuuki@gmail.com>
2021-04-22 15:57:41 -0400

    Adjust lims, check Symbols #12

--- case_control/volcano_plots.R
+++ case_control/volcano_plots.R
@@ -1,14 +1,13 @@
 
 library(tidyverse)
+library(EnhancedVolcano)
 library(here)
 library(sessioninfo)
-library(EnhancedVolcano)
-
 
 load(here("case_control","bipolarControl_deStats_byRegion_qSVAjoint_withAnnotation.rda"), verbose = TRUE)
 
 statOut_long <- as.data.frame(statOut) %>% 
-  select(starts_with("logFC"),starts_with("P.Val"),Type, Symbol) %>%
+  select(starts_with("logFC"),starts_with("adj.P.Val"),Type, Symbol) %>%
   rownames_to_column("mol") %>%
   pivot_longer(!c(mol,Type, Symbol)) %>% 
   separate(name,sep = "_", into = c("var","Region")) %>% 
@@ -17,17 +16,37 @@ statOut_long <- as.data.frame(statOut) %>%
 statOut_long$Type <- factor(statOut_long$Type, levels = c("Gene", "Exon", "Junction", "Transcript"))
 head(statOut_long)
 
-statOut_gene <- statOut_long %>% filter(Type == "Gene")
+statOut_long %>% group_by(Region, Type) %>% 
+  filter(abs(logFC) >1, adj.P.Val < 0.05) %>%
+  summarize(n = n(),
+            n_symb = paste(unique(Symbol), collapse = ", "))
+# # A tibble: 6 x 4
+# # Groups:   Region [2]
+# Region Type         n n_symb              
+# <chr>  <fct>    <int> <chr>               
+#   1 Amyg   Gene         1 SLC5A5              
+# 2 Amyg   Exon        14 IL1RL1, NR4A2, NR4A1
+# 3 Amyg   Junction     1 NR4A1               
+# 4 sACC   Gene         1 SERPINA3            
+# 5 sACC   Exon         2 SLC11A1, HAMP       
+# 6 sACC   Junction     2 HAMP, SLC11A1    
+
+statOut_long %>%
+  group_by(Region, Type) %>%
+  summarise(max_lfc = max(logFC),
+          min_lfc = min(logFC))
 
 en_volcano <-  EnhancedVolcano(statOut_long,
                                lab = statOut_long$Symbol,
                                  x = 'logFC',
-                                 y = 'P.Value') +
-  facet_grid(Region~Type)
+                                 y = 'adj.P.Val',
+                               pCutoff = 0.05) +
+  facet_grid(Region~Type) +
+  xlim(-2, 2)
 
 ggsave(en_volcano, filename = here("case_control","volcano_en.png"), width = 20, height = 10)
 
-# sgejobs::job_single('de_results_compare_regions', create_shell = TRUE, queue= 'bluejay', memory = '10G', command = "Rscript de_results_compare_regions.R")
+# sgejobs::job_single('volcano_plots', create_shell = TRUE, queue= 'bluejay', memory = '10G', command = "Rscript volcano_plots.R")
 ## Reproducibility information
 print("Reproducibility information:")
 Sys.time()
